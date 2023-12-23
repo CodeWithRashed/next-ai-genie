0bfaf8b1b4b88baa1c8b19b6fc3f1ac74f6f842a
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-23 23:01:40 +0600

    add-update / button banner

--- src/components/HomeBanner/HomeBanner.tsx
+++ src/components/HomeBanner/HomeBanner.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import bannerImage from "../../assets/banner.png";
+import { ButtonOutline, ButtonPrimary } from "../ui/Button";
 
 const HomeBanner = () => {
   return (
@@ -8,16 +9,15 @@ const HomeBanner = () => {
 
       <div className="grid lg:grid-cols-2 lg:gap-3 h-full lg:justify-between justify-center items-center">
         {/* Banner Text Content */}
-        <div className="text-center lg:text-left space-y-3 md:order-first order-last">
+        <div className="text-center lg:space-y-5 lg:text-left space-y-3 md:order-first order-last">
           <h1 className="leading-normal font-bold text-color-title text-6xl ">
             AI Genie One of the Best Generative <br />
             AI Tool
           </h1>
           <p className="text-color-subtitle">No Credit Card Required</p>
-          <div className="cta mt-5">
-            <button className="mt-5 bg-color-primary text-white px-4 py-2 rounded-main">
-              Try Demo
-            </button>
+          <div className="cta flex gap-3">
+           <ButtonPrimary >Free Trial</ButtonPrimary>
+           <ButtonOutline>Watch How It Works</ButtonOutline>
           </div>
         </div>
 
