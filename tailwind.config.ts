c6a37622d4c617cb835b7827eeda1afa0fa63383
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-23 19:33:15 +0600

    fix /css variable

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,13 +21,6 @@ const config: Config = {
         
           "color-primary": "var(--color-primary)",
           "color-primary-light": "var( --color-primary-light)",
-          "color-secondary": "var(--color-secondary)",
-          "btn-primary-bg": "var(--btn-primary-bg)",
-          "btn-secondary-bg": "var(--btn-secondary-bg)",
-          "btn-primary-text": "var(--btn-primary-text)",
-          "btn-secondary-text": "var(--btn-secondary-text)",
-          "card-primary-bg": "var(--card-primary-bg)",
-          "card-secondary-bg": "var(--card-secondary-bg)",
           "color-title": "var(--text-color-title)",
           "color-subtitle": "var(--text-color-subtitle)",
           "shadow-color": "var(--shadow-color)",
