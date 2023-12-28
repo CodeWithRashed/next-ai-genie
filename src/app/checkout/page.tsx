931ad874312adcbdf59584421bc737aa87f580b9
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-28 17:00:53 +0600

    add /checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,14 @@
+import Checkout from "@/components/Checkout/CheckoutPage"
+import Navbar from "@/components/NavigationMenus/DefaultNavbar/Navbar"
+
+
+const CheckOutPage = () => {
+  return (
+    <div className="mx-auto max-w-[1440px] px-5">
+      <Navbar/>
+      <Checkout/>
+    </div>
+  )
+}
+
+export default CheckOutPage
