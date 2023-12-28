a90751674baddcc083c3288631c74be0c3acd06c
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-28 17:08:02 +0600

    add /stripe

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,5 @@ export function middleware(request: NextRequest) {
 
 
 export const config = {
-  matcher: ["/", "/login", "/register", "/dashboard", "/dashboard/:path*"],
+  matcher: ["/", "/login", "/register","/checkout", "/dashboard", "/dashboard/:path*"],
 };
