140f29272ba3e0054123cd27e69dd13686339e7f
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-01 12:10:17 +0600

    improve /pricing card

--- src/components/Sections/PricingSection/PricingSection.tsx
+++ src/components/Sections/PricingSection/PricingSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaRegCheckCircle } from "react-icons/fa";
 
 const PricingSection = () => {
@@ -7,14 +8,14 @@ const PricingSection = () => {
         {/* Container */}
         <div className="grid lg:grid-cols-3 gap-10">
           {/* Free Card */}
-          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out">
+          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out duration-700">
             <div className="card-title space-y-5 ">
-              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left">
+              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left transition-all ease-in-out duration-700">
                 Free
               </h1>
 
               <h1>
-                <span className="text-4xl font-medium group-hover:text-white">
+                <span className="text-4xl font-medium group-hover:text-white transition-all ease-in-out duration-700">
                   $0/
                 </span>
                 mo
@@ -39,23 +40,23 @@ const PricingSection = () => {
                 </li>
               </ul>
             </div>
-            <div className="card-footer">
-              <button className="w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white">
+            <div className="card-footer w-full">
+              <Link href="/checkout?package=free" className="block text-center w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white duration-700">
                 Get Started
-              </button>
+              </Link>
             </div>
           </div>
 
           {/* Premium Package Card */}
-          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out">
+          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out duration-700">
             <div className="card-title space-y-5 ">
-              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left">
+              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left transition-all ease-in-out duration-700">
                 Premium
               </h1>
 
               <h1>
-                <span className="text-4xl font-medium group-hover:text-white">
-                  $9.99/
+                <span className="text-4xl font-medium group-hover:text-white transition-all ease-in-out duration-700">
+                  $9.00/
                 </span>
                 mo
               </h1>
@@ -80,23 +81,23 @@ const PricingSection = () => {
                 </li>
               </ul>
             </div>
-            <div className="card-footer">
-              <button className="w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white">
+            <div className="card-footer w-full">
+              <Link href="checkout?package=premium" className="block text-center w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white duration-700">
                 Get Started
-              </button>
+              </Link>
             </div>
           </div>
 
           {/* Golden Package Card */}
-          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out">
+          <div className="card group shadow-2xl hover:drop-shadow-2xl hover:scale-105 hover:text-white flex flex-col justify-between hover:bg-color-primary rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out duration-700">
             <div className="card-title space-y-5 ">
-              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left">
-                Premium
+              <h1 className="text-4xl font-bold text-color-primary group-hover:text-white text-left transition-all ease-in-out duration-700">
+                Golden
               </h1>
 
               <h1>
-                <span className="text-4xl font-medium group-hover:text-white">
-                  $19.99/
+                <span className="text-4xl font-medium group-hover:text-white transition-all ease-in-out duration-700">
+                  $19.00/
                 </span>
                 mo
               </h1>
@@ -121,10 +122,10 @@ const PricingSection = () => {
                 </li>
               </ul>
             </div>
-            <div className="card-footer">
-              <button className="w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white">
+            <div className="card-footer w-full">
+              <Link href="checkout?package=golden" className="block text-center w-full group-hover:text-color-title group-hover:bg-white px-5 py-3 rounded-main transition-all ease-in-out bg-color-primary text-white duration-700">
                 Get Started
-              </button>
+              </Link>
             </div>
           </div>
         </div>
