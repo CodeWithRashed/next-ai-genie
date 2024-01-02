3e0e6caf54f67d5b938ce2970ca9788e4f6135b1
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-02 16:28:09 +0600

    improve /user dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import aiGenieIcon from "../../assets/favicon.png";
-import GoldenPackage from "@/components/Sections/PricingSection/GoldenPackage";
-import { FaRegCheckCircle } from "react-icons/fa";
 import Link from "next/link";
 const DashboardPage = () => {
   const isPro = false;
   return (
-    <div className="h-[80vh] overflow-scroll grid grid-cols-2">
+    <div className="h-[80vh] overflow-scroll grid grid-cols-2 gap-5">
       <div className="flex flex-col">
         <div className="p-3 border border-color-primary shadow-lg rounded-main">
           <p>Current Package</p>
@@ -25,36 +23,63 @@ const DashboardPage = () => {
                   mo
                 </h1>
               </div>
-        
             </div>
           </div>
         </div>
       </div>
       <div>
-        {/* //Upgrade Card// */}
-        {!isPro && (
-          <div className="relative w-full mt-10 mx-auto rounded-main  bg-color-primary bg-gradient-to-t  from-[#4A25E1]  to-color-primary p-5 text-white text-center">
-            <div className="absolute left-1/2 -translate-x-1/2 -top-8 border-2 border-white p-2 bg-color-primary rounded-full mx-auto">
-              <Image
-                width={40}
-                height={40}
-                className="h-10 w-10"
-                src={aiGenieIcon}
-                alt="ai genie logo"
-              />
-            </div>
-
-            <div className="content mt-3">
-              <h1 className="text-lg">Go Unlimited with PRO</h1>
-            </div>
-
-            <div className="mt-4 flex gap-3 justify-center items-center mx-auto">
-              <button className="bg-color-primary rounded-main px-3 py-2">
-                Upgrade Now
-              </button>
+        <div>
+        <div className="p-3 border border-color-primary shadow-lg rounded-main">
+          <p>Prompt Details</p>
+          <div className="p-2 cursor-not-allowed">
+            {/* Golden Package Card */}
+            <div className="card shadow-xl  flex flex-col justify-between rounded-main border border-color-primary-light px-5 py-10 transition-all ease-in-out duration-700">
+              <div className="card-title flex gap-5 justify-center items-center">
+                <div className="total-prompt flex flex-col justify-center items-center">
+                  <h1 className=" border-b-2 border-b-color-primary">Total Prompt</h1>
+                <div className="mt-4 text-xl w-10 h-10 flex justify-center items-center rounded-full border-2 border-grey-bg font-bold text-color-primary  text-left transition-all ease-in-out duration-700">
+                 <p>1</p>
+                </div>
+                </div>
+                
+                <div className="available-prompt flex flex-col justify-center items-center">
+                <h1 className=" border-b-2 border-b-color-primary">Available Prompt</h1>
+                <div className="mt-4 text-xl w-10 h-10 flex justify-center items-center rounded-full border-2 border-grey-bg font-bold text-color-primary  text-left transition-all ease-in-out duration-700">
+                 <p>1</p>
+                </div>
+                </div>
+               
+              </div>
             </div>
           </div>
-        )}
+        </div>
+        </div>
+        <div>
+          {/* //Upgrade Card// */}
+          {!isPro && (
+            <div className="relative w-full mt-10 mx-auto rounded-main  bg-color-primary bg-gradient-to-t  from-[#4A25E1]  to-color-primary p-5 text-white text-center">
+              <div className="absolute left-1/2 -translate-x-1/2 -top-8 border-2 border-white p-2 bg-color-primary rounded-full mx-auto">
+                <Image
+                  width={40}
+                  height={40}
+                  className="h-10 w-10"
+                  src={aiGenieIcon}
+                  alt="ai genie logo"
+                />
+              </div>
+
+              <div className="content mt-3">
+                <h1 className="text-lg">Go Unlimited with PRO</h1>
+              </div>
+
+              <div className="mt-4 flex gap-3 justify-center items-center mx-auto">
+                <button className="bg-color-primary rounded-main px-3 py-2">
+                  Upgrade Now
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
