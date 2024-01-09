c449d46cdb83a671278726d6641138f4e00a432c
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-09 12:26:54 +0600

    add /article summarizer

--- src/app/dashboard/summarizer/page.tsx
+++ src/app/dashboard/summarizer/page.tsx
@@ -1,21 +1,14 @@
+import SummarizerPage from "@/components/DashboardPages/summarizer/SummarizerPage";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Ai Genie | Summarize Articles",
+  description: "Summarize Articles and Save Time",
+};
 const Summarizer = () => {
-  const bigData = false;
   return (
     <div>
-    <div
-      className={`result-container w-full h-[70vh] ${
-        bigData && "overflow-y-scroll"
-      } `}
-    ></div>
-
-    <form className="input-container h-16 relative">
-      <input
-        type="text"
-        className="h-10 bg-grey-bg rounded-main px-3 outline-none focus:border-color-primary  border border-color-primary focus:border-2 w-full  mt-5"
-      />
-      <button type="submit" className="absolute right-1 hover:bg-color-primary-dark rounded-main bottom-2 text-white bg-color-primary px-3 py-1">Submit</button>
-    </form>
+   <SummarizerPage/>
   </div>
   )
   }
