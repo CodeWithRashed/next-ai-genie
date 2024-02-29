c25b7f64b257b691f2296ce774d6234211f36224
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-29 20:44:54 +0600

    merged /payment history page

--- src/components/DashboardPages/user-dashboard/UserMainDashboard.tsx
+++ src/components/DashboardPages/user-dashboard/UserMainDashboard.tsx
@@ -4,12 +4,8 @@ import dynamic from "next/dynamic";
 import React from "react";
 import DashBoardLoading from "./DashBoardLoading";
 import DashboardDataCard from "./DashboardDataCard";
-
-const PaymentHistory = dynamic(() => import("./PaymentHistory"), {
-  ssr: false,
-});
-
 import ActiveFreeTrial from "./ActiveFreeTrial";
+import Link from "next/link";
 
 const ChartComp = dynamic(() => import("@/components/ChartsComp/ChartsComp"), {
   ssr: false,
@@ -18,6 +14,8 @@ const ChartComp = dynamic(() => import("@/components/ChartsComp/ChartsComp"), {
 const UserMainDashboard = () => {
   const [data, setData] = React.useState(null);
   const [isLoading, setLoading] = React.useState(true);
+  const [paymentData, setPaymentData] = React.useState(null);
+  const [customerPortalLink, setCustomerPortalLink] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api/get/packages")
@@ -26,20 +24,58 @@ const UserMainDashboard = () => {
         setData(data.packageData);
         setLoading(false);
       });
+    fetch("/api/get/payment")
+      .then((res) => res.json())
+      .then((data) => {
+        setPaymentData(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+    fetch("/api/get/payment?data=customer_portal_link")
+      .then((res) => res.json())
+      .then((data) => {
+        setCustomerPortalLink(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
   }, []);
 
-const useParentage = Math.round((data?.promptUsed / data?.promptCount) * 100)
-  if (isLoading)
+  const convertData = (timestamp) => {
+    let date = new Date(timestamp * 1000);
+
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    console.log(data);
+    console.log(timestamp);
+    console.log(date);
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  };
+
+  const useParentage = Math.round((data?.promptUsed / data?.promptCount) * 100);
+  if (isLoading) return <DashBoardLoading></DashBoardLoading>;
+  if (!data)
     return (
-      <DashBoardLoading></DashBoardLoading>
+      <div className="overflow-hidden">
+        <ActiveFreeTrial />
+      </div>
     );
-  if (!data) return <div className="overflow-hidden"><ActiveFreeTrial/></div>;
 
   return (
     <div className="bg-gray-100 rounded-main overflow-hidden p-3">
       <div className="header grid md:grid-cols-2 lg:grid-cols-3 gap-3 mb-5">
         <DashboardDataCard title="Current Package" data={data?.packageName} />
-        <DashboardDataCard title="Available Prompt" data={data?.promptCount - data?.promptUsed} />
+        <DashboardDataCard
+          title="Available Prompt"
+          data={data?.promptCount - data?.promptUsed}
+        />
         <DashboardDataCard title="Prompt Count" data={data?.promptCount} />
       </div>
 
@@ -48,11 +84,34 @@ const useParentage = Math.round((data?.promptUsed / data?.promptCount) * 100)
         <div className="shadow-md p-5 rounded-main h-full w-full bg-white">
           <p>Prompt History</p>
           <div>
-            <ChartComp series={[useParentage]}/>
+            <ChartComp series={[useParentage]} />
           </div>
         </div>
         <div className="shadow-md p-5 rounded-main h-full w-full bg-white flex flex-col justify-between">
-         <PaymentHistory/>
+          {/* PAYMENT HISTORY */}
+          <div>
+            <div>
+              <p>Payment History</p>
+              <div className="p-2 border mt-3 rounded-main">
+                <p className="font-medium">Payment Successful</p>
+                <p>Subscribed at: {convertData(paymentData?.plan?.created)}</p>
+                <p>
+                  Next Payment Cycle: {convertData(paymentData?.current_period_end)}
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <Link
+                className={`${
+                  !customerPortalLink && "cursor-not-allowed"
+                } underline underline-offset-4 hover:text-blue-500 transition-all ease-in-out`}
+                href={customerPortalLink || ""}
+              >
+                Manage Payment
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
